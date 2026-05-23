b2d5ce95d50f851b9411848c5d6fc6cac6e7283d
moodaylight <changyou7777@gmail.com>
2026-05-24 00:09:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -85,9 +85,9 @@ function getTextBounds(text){
 
         right: text.x + width / 2 + 10,
 
-       top: text.y - text.size + 10,
+       top: text.y - text.size + 5,
 
-bottom: text.y + 20 
+bottom: text.y + 15 
     };
 
 }
