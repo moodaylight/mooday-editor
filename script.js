23aa52edf7f0eda58766acabc381ea282b218dfc
moodaylight <changyou7777@gmail.com>
2026-05-27 22:53:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -212,31 +212,7 @@ function getTopText(x,y){
 // 双指距离
 // =====================
 
-function getDistance(t1,t2){
 
-    const dx = t2.clientX - t1.clientX;
-
-    const dy = t2.clientY - t1.clientY;
-
-    return Math.sqrt(dx * dx + dy * dy);
-
-}
-
-// =====================
-// 双指角度
-// =====================
-
-function getAngle(t1,t2){
-
-    return Math.atan2(
-
-        t2.clientY - t1.clientY,
-
-        t2.clientX - t1.clientX
-
-    ) * 180 / Math.PI;
-
-}
 
 // =====================
 // 图层面板
