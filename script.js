795bf389ec82923142144cc426f0dcb84284a92d
moodaylight <changyou7777@gmail.com>
2026-05-27 23:25:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -212,7 +212,31 @@ function getTopText(x,y){
 // 双指距离
 // =====================
 
+function getDistance(t1,t2){
 
+    const dx = t2.clientX - t1.clientX;
+
+    const dy = t2.clientY - t1.clientY;
+
+    return Math.sqrt(dx * dx + dy * dy);
+
+}
+
+// =====================
+// 双指角度
+// =====================
+
+function getAngle(t1,t2){
+
+    return Math.atan2(
+
+        t2.clientY - t1.clientY,
+
+        t2.clientX - t1.clientX
+
+    ) * 180 / Math.PI;
+
+}
 
 // =====================
 // 图层面板
@@ -753,10 +777,276 @@ ctx.restore();
 // Touch Start
 // =====================
 
+canvas.addEventListener("touchstart",(e)=>{
+
+    e.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+
+    // 双指初始化
+
+    if(e.touches.length === 2){
+
+        initialPinchDistance = getDistance(
+
+            e.touches[0],
+
+            e.touches[1]
+
+        );
+
+        initialRotationAngle = getAngle(
+
+            e.touches[0],
+
+            e.touches[1]
+
+        );
+
+        
+
+        if(imageSelected){
+
+            initialImageScale = imgScale;
+
+
+        }
+
+        return;
+
+    }
+
+    // 单指
+
+    const touch = e.touches[0];
+
+    const x = touch.clientX - rect.left;
+
+    const y = touch.clientY - rect.top;
+
+    // 判断文字
+
+    const clickedText = getTopText(x,y);
+
+    // 点击文字
+
+if(clickedText){
+
+    selectedText = clickedText;
+
+    imageSelected = false;
+
+    draggingText = true;
+
+    textOffsetX = x - clickedText.x;
+
+    textOffsetY = y - clickedText.y;
+
+    draggingImage = false;
+
+    // 长按进入编辑
+
+    longPressTimer = setTimeout(()=>{
+
+       if(draggingText){
+
+            textInput.style.display = "block";
+
+            textInput.value = clickedText.content;
+
+            textInput.focus();
+
+        }
+
+    },600);
+
+    draw();
+
+    return;
+
+}
+    // 点击空白
+
+imageSelected = true;
+
+selectedText = null;
+
+draggingImage = true;
+
+draggingText = false;
+
+dragOffsetX = x - imgX;
+
+dragOffsetY = y - imgY;
+
+
+
+draw();    
+
+});
 
 // 取消长按
 
+canvas.addEventListener("touchmove",(e)=>{
 
+    e.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+
+    // 单指拖动
+
+    if(e.touches.length === 1){
+
+        const touch = e.touches[0];
+
+        const x = touch.clientX - rect.left;
+
+        const y = touch.clientY - rect.top;
+
+        // 文字
+      
+        // 移动时取消长按
+    
+        if(draggingText && selectedText){
+      selectedText.x = x - textOffsetX
+       selectedText.y = y - textOffsetY
+       const box = getTextBounds(selectedText);
+
+// 左边界
+
+if(box.left < 48){
+
+    selectedText.x +=
+    48 - box.left;
+
+}
+
+// 右边界
+
+if(box.right > 312){
+
+    selectedText.x -=
+    box.right - 312;
+
+}
+
+// 上边界
+
+if(box.top < 45){
+
+    selectedText.y +=
+    45 - box.top;
+
+}
+
+// 下边界
+
+if(box.bottom > 480){
+
+    selectedText.y -=
+    box.bottom - 480;
+       
+
+        }
+
+        }
+
+        // 图片
+
+        if(draggingImage && imageSelected){
+
+    imgX = x - dragOffsetX;
+
+    imgY = y - dragOffsetY;
+    console.log("图片正在移动");
+}
+
+        draw();
+
+    }
+
+    // 双指缩放旋转
+
+    if(e.touches.length === 2){
+
+        const currentDistance = getDistance(
+
+            e.touches[0],
+
+            e.touches[1]
+
+        );
+
+        const currentAngle = getAngle(
+
+            e.touches[0],
+
+            e.touches[1]
+
+        );
+
+        const scale = currentDistance / initialPinchDistance;
+
+        const rotation = currentAngle - initialRotationAngle;
+
+
+        // 图片
+
+        if(imageSelected){
+
+    imgScale =
+    initialImageScale * scale;
+
+    // =====================
+    // 最小缩放限制（防露白）
+    // =====================
+
+    const frameWidth = 12;
+    const frameHeight = 17.5;
+
+    const visibleWidth = 9.8;
+    const visibleHeight = 15.3;
+
+    const marginX = 20;
+
+    const outerW =
+    canvas.width - marginX * 2;
+
+    const outerH =
+    canvas.height - marginX * 2;
+
+    const visibleRatioX =
+    visibleWidth / frameWidth;
+
+    const visibleRatioY =
+    visibleHeight / frameHeight;
+
+    const visibleW =
+    outerW * visibleRatioX;
+
+    const visibleH =
+    outerH * visibleRatioY;
+
+    // Cover最小值
+
+    const minScale =
+    Math.max(
+        visibleW / image.width,
+        visibleH / image.height
+    );
+
+    // 不允许缩小到露白
+
+    imgScale =
+    Math.max(imgScale, minScale);
+
+}
+
+        draw();
+
+    }
+
+});
 
 textInput.addEventListener("input",()=>{
 
