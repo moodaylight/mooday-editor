7c80a6b2059ea67449797fc7e753d92649ecb9f0
moodaylight <changyou7777@gmail.com>
2026-05-27 22:55:00 +0900

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,1 +1,26 @@
 touch.js
+function getDistance(t1,t2){
+
+    const dx = t2.clientX - t1.clientX;
+
+    const dy = t2.clientY - t1.clientY;
+
+    return Math.sqrt(dx * dx + dy * dy);
+
+}
+
+// =====================
+// 双指角度
+// =====================
+
+function getAngle(t1,t2){
+
+    return Math.atan2(
+
+        t2.clientY - t1.clientY,
+
+        t2.clientX - t1.clientX
+
+    ) * 180 / Math.PI;
+
+}
