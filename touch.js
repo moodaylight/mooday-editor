f070cf74660f547b9ac0b0377fa338199aa68a3b
moodaylight <changyou7777@gmail.com>
2026-05-27 23:03:36 +0900

    Update touch.js

--- touch.js
+++ touch.js
@@ -24,3 +24,111 @@ function getAngle(t1,t2){
     ) * 180 / Math.PI;
 
 }
+
+canvas.addEventListener("touchstart",(e)=>{
+
+    e.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+
+    // 双指初始化
+
+    if(e.touches.length === 2){
+
+        initialPinchDistance = getDistance(
+
+            e.touches[0],
+
+            e.touches[1]
+
+        );
+
+        initialRotationAngle = getAngle(
+
+            e.touches[0],
+
+            e.touches[1]
+
+        );
+
+        
+
+        if(imageSelected){
+
+            initialImageScale = imgScale;
+
+
+        }
+
+        return;
+
+    }
+
+    // 单指
+
+    const touch = e.touches[0];
+
+    const x = touch.clientX - rect.left;
+
+    const y = touch.clientY - rect.top;
+
+    // 判断文字
+
+    const clickedText = getTopText(x,y);
+
+    // 点击文字
+
+if(clickedText){
+
+    selectedText = clickedText;
+
+    imageSelected = false;
+
+    draggingText = true;
+
+    textOffsetX = x - clickedText.x;
+
+    textOffsetY = y - clickedText.y;
+
+    draggingImage = false;
+
+    // 长按进入编辑
+
+    longPressTimer = setTimeout(()=>{
+
+       if(draggingText){
+
+            textInput.style.display = "block";
+
+            textInput.value = clickedText.content;
+
+            textInput.focus();
+
+        }
+
+    },600);
+
+    draw();
+
+    return;
+
+}
+    // 点击空白
+
+imageSelected = true;
+
+selectedText = null;
+
+draggingImage = true;
+
+draggingText = false;
+
+dragOffsetX = x - imgX;
+
+dragOffsetY = y - imgY;
+
+
+
+draw();    
+
+});
