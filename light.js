c137b58ac5320e3f9c806e58af2962f1bd7d75c2
moodaylight <changyou7777@gmail.com>
2026-05-28 23:28:53 +0900

    Update light.js

--- light.js
+++ light.js
@@ -3,3 +3,31 @@
 // 暖光
 // 白光
 // 镜子模式
+const lightNames = [
+    "白光",
+    "暖光",
+    "日光",
+    "镜子"
+];
+
+lightModeBtn.innerText =
+lightNames[lightMode];
+
+lightModeBtn.addEventListener(
+"click",
+()=>{
+
+    lightMode++;
+
+    if(lightMode > 3){
+
+        lightMode = 0;
+
+    }
+
+    lightModeBtn.innerText =
+    lightNames[lightMode];
+
+    draw();
+
+});
