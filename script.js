f534547180f6b99cb1a970dbcdfa173a649c5507
moodaylight <changyou7777@gmail.com>
2026-06-02 01:27:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1265,7 +1265,106 @@ photoBtn.onclick = ()=>{
         Math.max(scaleX, scaleY);
 
     }
+texts.forEach(text=>{
 
+    const box =
+    getTextBounds(text);
+
+    const LIMIT = 10;
+
+    // 左边
+
+    if(
+        box.left <
+        exportVisibleX + LIMIT
+    ){
+
+        text.x +=
+        (
+            exportVisibleX +
+            LIMIT
+        )
+        -
+        box.left;
+
+    }
+
+    // 右边
+
+    if(
+        box.right >
+        exportVisibleX +
+        exportVisibleW -
+        LIMIT
+    ){
+
+        text.x -=
+        box.right
+        -
+        (
+            exportVisibleX +
+            exportVisibleW -
+            LIMIT
+        );
+
+    }
+
+    // 上边
+
+    if(
+        box.top <
+        exportVisibleY + LIMIT
+    ){
+
+        text.y +=
+        (
+            exportVisibleY +
+            LIMIT
+        )
+        -
+        box.top;
+
+    }
+
+    // 下边
+
+    if(
+        box.bottom >
+        exportVisibleY +
+        exportVisibleH -
+        LIMIT
+    ){
+
+        text.y -=
+        box.bottom
+        -
+        (
+            exportVisibleY +
+            exportVisibleH -
+            LIMIT
+        );
+
+    }
+
+    text.rx =
+
+    (
+        text.x -
+        exportVisibleX
+    )
+    /
+    exportVisibleW;
+
+    text.ry =
+
+    (
+        text.y -
+        exportVisibleY
+    )
+    /
+    exportVisibleH;
+
+});
     draw();
 
 };
@@ -1311,7 +1410,106 @@ mirrorBtn.onclick = ()=>{
         Math.max(scaleX, scaleY);
 
     }
+texts.forEach(text=>{
 
+    const box =
+    getTextBounds(text);
+
+    const LIMIT = 10;
+
+    // 左边
+
+    if(
+        box.left <
+        exportVisibleX + LIMIT
+    ){
+
+        text.x +=
+        (
+            exportVisibleX +
+            LIMIT
+        )
+        -
+        box.left;
+
+    }
+
+    // 右边
+
+    if(
+        box.right >
+        exportVisibleX +
+        exportVisibleW -
+        LIMIT
+    ){
+
+        text.x -=
+        box.right
+        -
+        (
+            exportVisibleX +
+            exportVisibleW -
+            LIMIT
+        );
+
+    }
+
+    // 上边
+
+    if(
+        box.top <
+        exportVisibleY + LIMIT
+    ){
+
+        text.y +=
+        (
+            exportVisibleY +
+            LIMIT
+        )
+        -
+        box.top;
+
+    }
+
+    // 下边
+
+    if(
+        box.bottom >
+        exportVisibleY +
+        exportVisibleH -
+        LIMIT
+    ){
+
+        text.y -=
+        box.bottom
+        -
+        (
+            exportVisibleY +
+            exportVisibleH -
+            LIMIT
+        );
+
+    }
+
+    text.rx =
+
+    (
+        text.x -
+        exportVisibleX
+    )
+    /
+    exportVisibleW;
+
+    text.ry =
+
+    (
+        text.y -
+        exportVisibleY
+    )
+    /
+    exportVisibleH;
+
+});
     draw();
 
 };
