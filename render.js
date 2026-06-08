af1f464d7b14cf489845853bf52dd681ad56fda6
moodaylight <changyou7777@gmail.com>
2026-06-09 00:09:16 +0900

    Update render.js

--- render.js
+++ render.js
@@ -159,19 +159,7 @@ imgY = Math.max(
 // 可编辑区域边框
 // =====================
 
-ctx.strokeStyle = "rgba(255,255,255,0.35)";
-
-ctx.lineWidth = 1.5;
-
-roundRect(
-    visibleX,
-    visibleY,
-    visibleW,
-    visibleH,
-    18
-);
-
-ctx.stroke();
+ctx.strokeStyle = "rgba(255,255,255,0)";
     ctx.clip();
 
 // =====================
