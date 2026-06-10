f6c27d339a5cb4e70c8e2ca7a9bf8b1c6fb900d2
moodaylight <changyou7777@gmail.com>
2026-06-10 17:53:20 +0900

    Image Module Step6

--- image.js
+++ image.js
@@ -23,3 +23,64 @@ let imgRotation = 0;
 let imageSelected = false;
 
 let initialImageScale = 1;
+
+function fitImageCover(){
+
+    if(!image) return;
+
+    let frameWidth;
+    let frameHeight;
+
+    let visibleWidth;
+    let visibleHeight;
+
+    if(productType === "mirror"){
+
+        frameWidth = 12;
+        frameHeight = 17.5;
+
+        visibleWidth = 9.8;
+        visibleHeight = 15.3;
+
+    }
+
+    if(productType === "photo"){
+
+        frameWidth = 4;
+        frameHeight = 6;
+
+        visibleWidth = 4;
+        visibleHeight = 6;
+
+    }
+
+    const marginX = 20;
+
+    const outerW =
+    canvas.width - marginX * 2;
+
+    const outerH =
+    canvas.height - marginX * 2;
+
+    const visibleRatioX =
+    visibleWidth / frameWidth;
+
+    const visibleRatioY =
+    visibleHeight / frameHeight;
+
+    const visibleW =
+    outerW * visibleRatioX;
+
+    const visibleH =
+    outerH * visibleRatioY;
+
+    const scaleX =
+    visibleW / image.width;
+
+    const scaleY =
+    visibleH / image.height;
+
+    imgScale =
+    Math.max(scaleX, scaleY);
+
+}
