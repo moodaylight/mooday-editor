93c1c14819e70074419cd3ee072a67c1ea33a6ca
moodaylight <changyou7777@gmail.com>
2026-06-10 21:00:52 +0900

    Touch Module Step5

--- touch.js
+++ touch.js
@@ -14,3 +14,24 @@ function stopDragging(){
     draggingImage = false;
 
 }
+function selectImageMode(){
+
+    imageSelected = true;
+
+    clearSelectedText();
+
+    textEditorPanel.style.display = "none";
+
+    fontPanel.style.display = "none";
+
+    colorPanel.style.display = "none";
+
+    sizePanel.style.display = "none";
+
+    strokePanel.style.display = "none";
+
+    shadowPanel.style.display = "none";
+
+    opacityPanel.style.display = "none";
+
+}
