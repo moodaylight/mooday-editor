2e23b133c082f17137773d0cd17c3ec1ce4dbb12
moodaylight <changyou7777@gmail.com>
2026-06-14 03:10:46 +0900

    Create productConfig.js

---
+++ productConfig.js
@@ -0,0 +1,99 @@
+const PRODUCTS = {
+
+    photo:{
+
+        name:"일반 사진",
+
+        ratio:"2:3",
+
+        buttons:[
+
+            "upload",
+            "clarity",
+            "frame",
+
+            "mood1",
+            "mood2",
+            "mood3",
+
+            "mood4",
+            "mood5",
+            "mood6"
+
+        ]
+
+    },
+
+    frame:{
+
+        name:"액자 사진",
+
+        ratio:"2:3",
+
+        buttons:[
+
+            "upload",
+            "clarity",
+            "mood",
+
+            "frame",
+            "text",
+            "background",
+
+            "poster",
+            "preview",
+            "submit"
+
+        ]
+
+    },
+
+    idcard:{
+
+        name:"증명사진",
+
+        ratio:"2:3",
+
+        buttons:[
+
+            "upload",
+            "clarity",
+            "background",
+
+            "size",
+            "guide",
+            "text",
+
+            "preview",
+            "save",
+            "submit"
+
+        ]
+
+    },
+
+    mirror:{
+
+        name:"매직미러",
+
+        ratio:"12:17.5",
+
+        buttons:[
+
+            "upload",
+            "clarity",
+            "frame",
+
+            "text",
+            "background",
+            "poster",
+
+            "light",
+            "preview",
+            "submit"
+
+        ]
+
+    }
+
+};
