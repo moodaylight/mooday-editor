4509cf58ed420c19faf98e1465ea9102844b9c5e
moodaylight <changyou7777@gmail.com>
2026-06-17 23:24:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -697,35 +697,7 @@ image.onload = function(){
 
 if(frameBtn){
 
-frameBtn.onclick = ()=>{
 
-    decorations.push({
-
-        content:
-        decoList[decoIndex],
-
-        x:200 + decoIndex * 20,
-
-        y:200 + decoIndex * 20,
-
-        size:28
-
-    });
-
-    decoIndex++;
-
-    if(
-        decoIndex >=
-        decoList.length
-    ){
-
-        decoIndex = 0;
-
-    }
-
-    draw();
-
-};
 
 }
 
