95cb39d6d94a67505bd12f50316578ad3be3630e
moodaylight <changyou7777@gmail.com>
2026-06-17 23:26:11 +0900

    Update frameConfig.js

--- frameConfig.js
+++ frameConfig.js
@@ -1,3 +1,32 @@
 const FRAMES = {
 
 };
+frameBtn.onclick = ()=>{
+
+    decorations.push({
+
+        content:
+        decoList[decoIndex],
+
+        x:200 + decoIndex * 20,
+
+        y:200 + decoIndex * 20,
+
+        size:28
+
+    });
+
+    decoIndex++;
+
+    if(
+        decoIndex >=
+        decoList.length
+    ){
+
+        decoIndex = 0;
+
+    }
+
+    draw();
+
+};
