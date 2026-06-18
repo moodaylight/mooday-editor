55ed68fa7e597ca50aa18a3a6352e65a4d9c2fd1
moodaylight <changyou7777@gmail.com>
2026-06-18 23:03:56 +0900

    Update resizeCanvas.js

--- resizeCanvas.js
+++ resizeCanvas.js
@@ -1,1 +1,19 @@
+function resizeCanvas(){
 
+    canvas.width = canvas.offsetWidth;
+
+    canvas.height = canvas.offsetHeight;
+
+    if(image){
+
+        imgX = canvas.width / 2;
+
+        imgY = canvas.height / 2;
+
+        fitImageCover();
+
+    }
+
+    draw();
+
+}
