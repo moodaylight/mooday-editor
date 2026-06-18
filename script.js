bfaefefff36c2afcf623b8794dd936f9e7820788
moodaylight <changyou7777@gmail.com>
2026-06-18 22:32:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1554,60 +1554,6 @@ banners[bannerIndex].desc;
 },4000);
 let currentProduct = null;
 
-function selectProduct(type){
-
-    currentProduct = type;
-    currentConfig =
-    PRODUCTS[type];
-   updateButtons();
-   console.log(
-currentConfig.buttons
-);
-    updateOptions();
-    document.getElementById(
-        "homePage"
-    ).style.display = "none";
-
-    document.getElementById(
-        "editorContainer"
-    ).style.display = "block";
-
-    document.getElementById(
-        "editorPage"
-    ).style.display = "flex";
-
-    document.getElementById(
-        "bottomPanel"
-    ).style.display = "flex";
-
-    resizeCanvas();
-
-    if(type === "photo"){
-
-        photoBtn.click();
-
-    }
-
-    if(type === "frame"){
-
-        photoBtn.click();
-
-    }
-
-    if(type === "idcard"){
-
-        photoBtn.click();
-
-    }
-
-    if(type === "mirror"){
-
-        mirrorBtn.click();
-
-    }
-
-}
-
 function goHome(){
 
     document.getElementById(
