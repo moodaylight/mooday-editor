3e13a90dfd8e5ff1120bd5b90892d22681a819c6
moodaylight <changyou7777@gmail.com>
2026-06-19 22:26:14 +0900

    Update exportManager.js

--- exportManager.js
+++ exportManager.js
@@ -1,1 +1,20 @@
 
+previewBtn.onclick = ()=>{
+
+    draw();
+
+    const link =
+    document.createElement("a");
+
+    link.download =
+    "4x6-photo.jpg";
+
+    link.href =
+    canvas.toDataURL(
+        "image/jpeg",
+        1.0
+    );
+
+    link.click();
+
+};
