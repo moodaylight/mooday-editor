beb55b3e1941bb2a15a5af47288106552b0293b9
moodaylight <changyou7777@gmail.com>
2026-06-19 20:29:49 +0900

    Update textEditor.js

--- textEditor.js
+++ textEditor.js
@@ -68,3 +68,65 @@ sizeBtn.onclick = ()=>{
     }
 
 };
+sizeMinus.onclick = ()=>{
+
+    if(!selectedText) return;
+
+    selectedText.size =
+    Math.max(
+        8,
+        (selectedText.size || 28) - 2
+    );
+
+    sizeValue.innerText =
+    selectedText.size;
+
+    draw();
+
+};
+
+sizePlus.onclick = ()=>{
+
+    if(!selectedText) return;
+
+    const oldSize =
+    selectedText.size || 28;
+
+    selectedText.size =
+    oldSize + 2;
+
+    const box =
+    getTextBounds(selectedText);
+
+    const LIMIT = 10;
+
+    if(
+
+        box.left < exportVisibleX + LIMIT ||
+
+        box.right >
+        exportVisibleX +
+        exportVisibleW -
+        LIMIT ||
+
+        box.top < exportVisibleY + LIMIT ||
+
+        box.bottom >
+        exportVisibleY +
+        exportVisibleH -
+        LIMIT
+
+    ){
+
+        // 超出边界
+        selectedText.size =
+        oldSize;
+
+    }
+
+    sizeValue.innerText =
+    selectedText.size;
+
+    draw();
+
+};
