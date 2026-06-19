1d4c09f0f334e7275548bac1d350939695d2ad68
moodaylight <changyou7777@gmail.com>
2026-06-19 22:13:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -364,36 +364,7 @@ previewBtn.onclick = ()=>{
 
 let templateLongPressTimer;
 let templateLongPressed = false;
-moodBtn.onclick = ()=>{
 
-    moodMode++;
-
-    if(moodMode > 4){
-
-        moodMode = 0;
-
-    }
-
-   const moods = [
-
-    "원본",
-
-    "따뜻한 기억",
-
-    "영화 같은 순간",
-
-    "고요한 일상",
-
-    "여행 기록"
-
-];
-
-    moodBtn.innerText =
-    moods[moodMode];
-
-    draw();
-
-};
 
 
 
