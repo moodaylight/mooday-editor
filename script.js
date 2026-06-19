1e9bd5a1724b99635e6bb3698e9719cd76cbf191
moodaylight <changyou7777@gmail.com>
2026-06-19 20:25:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -573,34 +573,7 @@ editorInput.addEventListener(
 }
 );
 
-sizeBtn.onclick = ()=>{
 
-    sizePanel.style.display =
-    "flex";
-
-    fontPanel.style.display =
-    "none";
-
-    colorPanel.style.display =
-    "none";
-
-    strokePanel.style.display =
-    "none";
-
-    shadowPanel.style.display =
-    "none";
-
-    opacityPanel.style.display =
-    "none";
-
-    if(selectedText){
-
-        sizeValue.innerText =
-        selectedText.size || 28;
-
-    }
-
-};
 
 
 strokeBtn.onclick = ()=>{
