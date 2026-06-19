d603d84f17b4186185c832533654b2dadb0b357b
moodaylight <changyou7777@gmail.com>
2026-06-19 19:48:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -930,27 +930,7 @@ sizeMinus.ontouchend = ()=>{
     clearInterval(sizeTimer);
 
 };
-fontBtn.onclick = ()=>{
 
-    fontPanel.style.display =
-    "flex";
-
-    colorPanel.style.display =
-    "none";
-
-    sizePanel.style.display =
-    "none";
-
-    strokePanel.style.display =
-    "none";
-
-    shadowPanel.style.display =
-    "none";
-
-    opacityPanel.style.display =
-    "none";
-
-};
 
 document
 .querySelectorAll(
