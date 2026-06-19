6a7529ef6c0c74fc1f929b76d144c761f6a77116
moodaylight <changyou7777@gmail.com>
2026-06-19 22:14:03 +0900

    Update styleEditor.js

--- styleEditor.js
+++ styleEditor.js
@@ -1,3 +1,33 @@
 function applyStyle(id){
 
 }
+moodBtn.onclick = ()=>{
+
+    moodMode++;
+
+    if(moodMode > 4){
+
+        moodMode = 0;
+
+    }
+
+   const moods = [
+
+    "원본",
+
+    "따뜻한 기억",
+
+    "영화 같은 순간",
+
+    "고요한 일상",
+
+    "여행 기록"
+
+];
+
+    moodBtn.innerText =
+    moods[moodMode];
+
+    draw();
+
+};
