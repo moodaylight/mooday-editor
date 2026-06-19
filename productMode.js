1195a5812cc025997ba4bf29ae1b1bf271cda553
moodaylight <changyou7777@gmail.com>
2026-06-19 19:24:07 +0900

    Update productMode.js

--- productMode.js
+++ productMode.js
@@ -1,1 +1,25 @@
 
+photoBtn.onclick = ()=>{
+
+    productType = "photo";
+    lightMode = 0;
+
+    lightModeBtn.style.display =
+    "none";
+
+    if(image){
+
+        fitImageCover();
+
+    }
+
+    draw();
+
+    texts.forEach(text=>{
+
+        ...
+    });
+
+    draw();
+
+};
