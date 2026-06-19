11a4d435504b8bcf5df5c3a28aed5264a7720a28
moodaylight <changyou7777@gmail.com>
2026-06-19 20:29:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -727,68 +727,7 @@ opacityPlus.onclick = ()=>{
     draw();
 
 };
-sizeMinus.onclick = ()=>{
 
-    if(!selectedText) return;
-
-    selectedText.size =
-    Math.max(
-        8,
-        (selectedText.size || 28) - 2
-    );
-
-    sizeValue.innerText =
-    selectedText.size;
-
-    draw();
-
-};
-
-sizePlus.onclick = ()=>{
-
-    if(!selectedText) return;
-
-    const oldSize =
-    selectedText.size || 28;
-
-    selectedText.size =
-    oldSize + 2;
-
-    const box =
-    getTextBounds(selectedText);
-
-    const LIMIT = 10;
-
-    if(
-
-        box.left < exportVisibleX + LIMIT ||
-
-        box.right >
-        exportVisibleX +
-        exportVisibleW -
-        LIMIT ||
-
-        box.top < exportVisibleY + LIMIT ||
-
-        box.bottom >
-        exportVisibleY +
-        exportVisibleH -
-        LIMIT
-
-    ){
-
-        // 超出边界
-        selectedText.size =
-        oldSize;
-
-    }
-
-    sizeValue.innerText =
-    selectedText.size;
-
-    draw();
-
-};
 // 长按连续缩小放大
 
 let sizeTimer = null;
