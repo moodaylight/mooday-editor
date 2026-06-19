4d666267e377027a3cb0e7e981924f5119169bef
moodaylight <changyou7777@gmail.com>
2026-06-19 19:49:07 +0900

    Update textEditor.js

--- textEditor.js
+++ textEditor.js
@@ -1,1 +1,21 @@
+fontBtn.onclick = ()=>{
 
+    fontPanel.style.display =
+    "flex";
+
+    colorPanel.style.display =
+    "none";
+
+    sizePanel.style.display =
+    "none";
+
+    strokePanel.style.display =
+    "none";
+
+    shadowPanel.style.display =
+    "none";
+
+    opacityPanel.style.display =
+    "none";
+
+};
