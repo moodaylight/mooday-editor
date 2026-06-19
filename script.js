567ad9fc3508f865701763e248ad31cc8c3795df
moodaylight <changyou7777@gmail.com>
2026-06-19 22:25:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -341,25 +341,7 @@ draw();
 
 
 
-previewBtn.onclick = ()=>{
 
-    draw();
-
-    const link =
-    document.createElement("a");
-
-    link.download =
-    "4x6-photo.jpg";
-
-    link.href =
-    canvas.toDataURL(
-        "image/jpeg",
-        1.0
-    );
-
-    link.click();
-
-};
 
 
 let templateLongPressTimer;
