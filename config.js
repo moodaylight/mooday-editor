7e36e8795485e1790b5416c820b2b08e445e494e
moodaylight <changyou7777@gmail.com>
2026-06-21 00:00:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,50 @@
 // 尺寸
 // 边距
 // 默认参数
+// =====================
+// ID PHOTO CONFIG
+// =====================
+
+const ID_PHOTO_CONFIG = {
+
+    "한국 증명사진":{
+        width:30,
+        height:40
+    },
+
+    "한국 여권":{
+        width:35,
+        height:45
+    },
+
+    "중국 여권":{
+        width:33,
+        height:48
+    },
+
+    "한국 비자":{
+        width:35,
+        height:45
+    },
+
+    "중국 신분증":{
+        width:26,
+        height:32
+    },
+
+    "중국 1인치":{
+        width:28,
+        height:35
+    },
+
+    "중국 2인치":{
+        width:35,
+        height:45
+    },
+
+    "미국 여권":{
+        width:51,
+        height:51
+    }
+
+};
