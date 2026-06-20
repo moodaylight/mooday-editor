be8a175e46add6049097028a39139d330668f6dc
moodaylight <changyou7777@gmail.com>
2026-06-21 00:10:50 +0900

    Update resizeCanvas.js

--- resizeCanvas.js
+++ resizeCanvas.js
@@ -1,14 +1,86 @@
 function resizeCanvas(){
 
-    canvas.width = canvas.offsetWidth;
+    const wrap =
+    document.querySelector(
+        ".canvas-wrap"
+    );
 
-    canvas.height = canvas.offsetHeight;
+    if(
+        currentProduct ===
+        "idcard"
+    ){
+
+        const config =
+        ID_PHOTO_CONFIG[
+            currentIdPhoto
+        ];
+
+        if(config){
+
+            const ratio =
+
+            config.width /
+            config.height;
+
+            let width =
+
+            wrap.offsetWidth;
+
+            let height =
+
+            width /
+            ratio;
+
+            const maxHeight =
+
+            window.innerHeight *
+            0.65;
+
+            if(
+                height >
+                maxHeight
+            ){
+
+                height =
+                maxHeight;
+
+                width =
+                height *
+                ratio;
+
+            }
+
+            canvas.style.width =
+            width + "px";
+
+            canvas.style.height =
+            height + "px";
+
+        }
+
+    }else{
+
+        canvas.style.width =
+        "";
+
+        canvas.style.height =
+        "";
+
+    }
+
+    canvas.width =
+    canvas.offsetWidth;
+
+    canvas.height =
+    canvas.offsetHeight;
 
     if(image){
 
-        imgX = canvas.width / 2;
+        imgX =
+        canvas.width / 2;
 
-        imgY = canvas.height / 2;
+        imgY =
+        canvas.height / 2;
 
         fitImageCover();
 
