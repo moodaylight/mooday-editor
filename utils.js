b0fa0afdece98946e2234eaceda1a78bc1599dc2
moodaylight <changyou7777@gmail.com>
2026-06-20 16:26:02 +0900

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,42 @@
 // =====================
 // UTILS MODULE
 // =====================
+// 是否点中文字
+// =====================
+
+function pointInText(text,x,y){
+
+    const box = getTextBounds(text);
+
+    return (
+
+        x >= box.left &&
+        x <= box.right &&
+        y >= box.top &&
+        y <= box.bottom
+
+    );
+
+}
+
+// =====================
+// 获取顶部文字
+// =====================
+
+function getTopText(x,y){
+
+    for(let i = texts.length - 1; i >= 0; i--){
+
+ if(pointInText(texts[i],x,y)){
+
+    selectedTextIndex = i;
+
+    return texts[i];
+
+}       
+
+    }
+
+    return null;
+
+}
