116f5467a66d8ca5a5e780c950296ac0dc9cde1a
moodaylight <changyou7777@gmail.com>
2026-06-20 23:20:03 +0900

    Create idPhotoConfig.js

---
+++ idPhotoConfig.js
@@ -0,0 +1,47 @@
+// =====================
+// ID PHOTO CONFIG
+// =====================
+
+const ID_PHOTO_CONFIG = {
+
+    korea_id:{
+        width:30,
+        height:40
+    },
+
+    korea_passport:{
+        width:35,
+        height:45
+    },
+
+    china_passport:{
+        width:33,
+        height:48
+    },
+
+    korea_visa:{
+        width:35,
+        height:45
+    },
+
+    china_idcard:{
+        width:26,
+        height:32
+    },
+
+    china_1inch:{
+        width:28,
+        height:35
+    },
+
+    china_2inch:{
+        width:35,
+        height:45
+    },
+
+    usa_passport:{
+        width:51,
+        height:51
+    }
+
+};
