bae98d20b429848bd35465504efaf38bae2975e5
moodaylight <changyou7777@gmail.com>
2026-06-21 16:22:35 +0900

    Create guideConfig.js

---
+++ guideConfig.js
@@ -0,0 +1,15 @@
+const GUIDE_CONFIG = {
+
+    "한국 여권":{
+        type:"headHeight",
+        min:32,
+        max:36
+    },
+
+    "미국 비자":{
+        type:"headHeight",
+        min:25,
+        max:35
+    }
+
+};
