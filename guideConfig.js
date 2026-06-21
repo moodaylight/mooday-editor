33b4a410577210d3e99bf9daaff63b8b8d973bcd
moodaylight <changyou7777@gmail.com>
2026-06-21 16:31:11 +0900

    Update guideConfig.js

--- guideConfig.js
+++ guideConfig.js
@@ -1,9 +1,33 @@
 const GUIDE_CONFIG = {
 
+    "한국 증명사진":{
+        type:"normal"
+    },
+
     "한국 여권":{
         type:"headHeight",
         min:32,
         max:36
+    },
+
+    "중국 여권":{
+        type:"chinaPassport"
+    },
+
+    "한국 비자":{
+        type:"normal"
+    },
+
+    "중국 신분증":{
+        type:"normal"
+    },
+
+    "중국 1인치":{
+        type:"normal"
+    },
+
+    "중국 2인치":{
+        type:"normal"
     },
 
     "미국 비자":{
