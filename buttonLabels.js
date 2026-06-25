80f228474e77fbb367c36fc4ccfb69be88982fb2
moodaylight <changyou7777@gmail.com>
2026-06-25 22:08:57 +0900

    Update buttonLabels.js

--- buttonLabels.js
+++ buttonLabels.js
@@ -2,31 +2,25 @@ const BUTTON_LABELS = {
 
     upload:"업로드",
 
-    clarity:"선명도",
+    enhance:"원클릭",
 
-    enhance:"원클릭 보정",
+    background:"배경",
+
+    guide:"안내",
 
     mood:"분위기",
 
-    frame:"프레임",
-
     text:"문자",
 
-    background:"배경",
+    frame:"프레임",
 
     style:"스타일",
 
     light:"조명",
 
-    optimize:"보정",
-
-    adjust:"조절",
-
     preview:"미리보기",
 
     submit:"주문하기",
-
-    download:"다운로드",
 
     home:"홈"
 
